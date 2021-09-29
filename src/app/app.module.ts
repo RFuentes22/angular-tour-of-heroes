6147b0e82bfb916ab75e2836349ca4ae079c3cdf
Rober Fuentes <00062217@uca.edu.sv>
2021-09-28 18:59:41 -0600

    bug fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,20 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent,
+    MessagesComponent
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule {}
+export class AppModule { }
